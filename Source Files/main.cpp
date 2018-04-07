01fe8adea1893efc40428b42126b20668d30790b
rogueAvocado <giorgiocassata@yahoo.com>
2018-04-07 17:22:20 -0600

    Visual Studio cont.

--- Source Files/main.cpp
+++ Source Files/main.cpp
@@ -1,9 +1,18 @@
+/*
+	not all of these sfml things are used yet, but they
+	might at some point so here they are.
+*/
 #include <SFML/Graphics.hpp>
+#include <SFML/Main.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Config.hpp>
+#include <SFML/Network.hpp>
+#include <SFML/System.hpp>
+
 #include <iostream>
 #include <vector>
 #include <string>
-#include <cmath>
-#include <ctime>
 #include <cstdlib>
 
 //#include "any header files.h"
@@ -13,25 +22,40 @@ using namespace std;
 using namespace sf;
 
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
+int main() {
+	//define stuff
+    sf::RenderWindow game(sf::VideoMode(640, 400), "tidePods");
+	sf::Clock clock;
+	
+	//game settings
+	game.setFramerateLimit(120); // Limit to 60 frames per second
+	game.setVerticalSyncEnabled(false); // False by default
+
+	// a sample shape
+	sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
-    while (window.isOpen())
-    {
+
+    while (game.isOpen()) {
+		//process events
         sf::Event event;
-        while (window.pollEvent(event))
-        {
+        while (game.pollEvent(event)) {
+			//close window
             if (event.type == sf::Event::Closed)
-                window.close();
+                game.close();
         }
 
+		// Get elapsed time since last loop
+		sf::Time elapsed1 = clock.getElapsedTime();
+		float FrameRate = 1.f / elapsed1.asSeconds();
+		std::cout << FrameRate << std::endl;
+		clock.restart();
 
-        window.clear();
-        window.draw(shape);
-        window.display();
+
+		//clear --> draw --> display
+        game.clear();
+        game.draw(shape);
+        game.display();
     }
 
     return 0;
--- TODO.md
+++ TODO.md
@@ -1,3 +1,7 @@
 Make the entire program pretty much
 
 get SFML and Visual to work together well for all dev peeps
+
+Note:	SFML website is not entirely up to date, some functions
+		have changed their names. Most do not start with capital 
+		letters anymore.
