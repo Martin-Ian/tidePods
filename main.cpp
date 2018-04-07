165d84f758439f172dece8f47bf381efaf6b667c
rogueAvocado <giorgiocassata@yahoo.com>
2018-04-07 15:40:03 -0600

    update

--- main.cpp
+++ main.cpp
@@ -1,6 +1,37 @@
-import <iostream>
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <ctime>
+#include <cstdlib>
+
+
+
+
 using namespace std;
-int main(){
-  cout << "spaghetti" << endl;
-return 0;
+using namespace sf;
+
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
 }
